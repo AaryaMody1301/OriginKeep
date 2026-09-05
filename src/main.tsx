e035b4ac00e8ffd7f074774a426422013a2dcf89
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-05 15:55:47 +0530

    Expose existing-file adoption in OriginKeep 2.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AdoptExisting from "./AdoptExisting";
 import OriginKeep2 from "./OriginKeep2";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <OriginKeep2 />
+    <AdoptExisting />
   </StrictMode>,
 );
