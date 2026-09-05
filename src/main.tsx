bb347e9d0832ff09518f36cfafcc9d7d36e58f9e
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-05 19:06:15 +0530

    Expose existing-file adoption in desktop workspace

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AdoptFilePanel from "./AdoptFilePanel";
 import App from "./App";
 import PassportDashboard from "./PassportDashboard";
 import "./styles.css";
@@ -7,6 +8,7 @@ import "./styles.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PassportDashboard />
+    <AdoptFilePanel />
     <App />
   </StrictMode>,
 );
