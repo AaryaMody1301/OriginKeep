9917c5296a51164cdd1efb72fd9983c36f9da762
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-05 15:02:20 +0530

    Load File Passport workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import PassportCenter from "./PassportCenter";
 import "./styles.css";
+import "./passport.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
