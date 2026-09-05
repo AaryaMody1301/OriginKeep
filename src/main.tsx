ff96275b66b01405e2a4b477d0f5c140a1b70a0b
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-09-05 15:54:29 +0530

    Launch the OriginKeep 2.0 Passport workspace

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import OriginKeep2 from "./OriginKeep2";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <OriginKeep2 />
   </StrictMode>,
 );
